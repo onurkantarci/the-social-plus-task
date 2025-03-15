b21b967bc4e8a8846f14bd5fcdaee6811a18fd60
onurkantarci <onurkntrc@gmail.com>
2025-03-15 20:34:01 +0300

    add User and Post types

---
+++ src/types.ts
@@ -0,0 +1,25 @@
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
+
+export interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
